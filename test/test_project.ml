a82d4d94b330a1f21eea963d0cb49e650c69488d
Pedro Cardoso <pedrodamiao18@gmail.com>
2026-03-26 17:57:37 +0000

    testes valkey

--- test/test_project.ml
+++ test/test_project.ml
@@ -1,39 +1,53 @@
 open Lwt.Infix
 open Redis_lwt
+open Yojson.Basic.Util
 
 
 (* let job =
   {|{
     "language": "ocaml",
-    "code": "print_endline "OLaaaaaaaaaaaaaa""
-  }|}
+    "code": "print_endline \"Hello World!\""
+    }|}
+    
+    let () =
+    
+    let conn = Client.connect {host= "127.0.0.1"; port= 6379} in
+    let result json =
+      conn >>= fun c -> Client.lpush c "jobs" [json]
+    in
+    let pong = Lwt_main.run (result job) in
+    Printf.printf "%i\n" pong *)
+    
+    let parse_job job =
+      try
+        let json = Yojson.Basic.from_string job in
+        let language = json |> member "language" |> to_string in
+        let code = json |> member "code" |> to_string in
+        Some (language, code)
+      with _ ->
+        None 
 
-let () =
-  
-  let conn = Client.connect {host= "127.0.0.1"; port= 6379} in
-  let result json =
-    conn >>= fun c -> Client.lpush c "jobs" [json]
-  in
-  let pong = Lwt_main.run (result job) in
-  Printf.printf "%i\n" pong *)
-
-let rec worker conn =
-  Client.brpop conn ["jobs"] 0 >>= function
-  | None ->
-      worker conn
-  | Some (_queue, job) ->
-      Printf.printf "Job recebido: %s\n%!" job;
-      worker conn
-
-let () =
-  let main =
-    Client.connect {host = "127.0.0.1"; port = 6379} >>= fun conn ->
-    worker conn
-  in
-  Lwt_main.run main
-
-
-
-(* let conn = Client.connect{host = "127.0.0.1"; port = 6379} in let result =
-   conn >>= fun c -> Redis_lwt.Client.ping c in let pong = Lwt_main.run
-   result in Printf.printf "%b\n" pong *)
+     let rec worker conn =
+      Client.brpop conn ["jobs"] 0 >>= function
+      | None ->
+        worker conn
+        | Some (_queue, job) ->
+          match parse_job job with
+          | None ->
+            Printf.printf "Erro ao fazer parse do JSON\n%!";
+            worker conn
+            | Some (language, code) ->
+              Printf.printf "Linguagem: %s\n" language;
+              Printf.printf "Código: %s\n%!" code;
+              worker conn
+              
+              
+              let () =
+              let main =
+                Client.connect {host = "127.0.0.1"; port = 6379} >>= fun conn ->
+                  worker conn
+                in
+                Lwt_main.run main
+                
+                
+                
