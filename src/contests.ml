bbd62d85de3edf5bbb71411192760d4a03081da7
Pedro Casais <152277946+pedrocasais@users.noreply.github.com>
2026-04-11 20:23:42 +0100

    Add routes for auth, contests, users with db connection pool

--- src/contests.ml
+++ src/contests.ml
@@ -1,18 +1,39 @@
-let getContestsIdScoreboard _request =  
- failwith  "getContestsIdScoreboard" 
-let getContestsContestIdSubmissions _request =  
- failwith  "getContestsContestIdSubmissions" 
-let postContestsContestsIdProblems _request =  
- failwith  "postContestsContestsIdProblems" 
-let getContestsContestsIdProblems _request =  
- failwith  "getContestsContestsIdProblems" 
-let deleteContestsId _request =  
- failwith  "deleteContestsId" 
-let putContestsId _request =  
- failwith  "putContestsId" 
-let getContestsId _request =  
- failwith  "getContestsId" 
-let postContests _request =  
- failwith  "postContests" 
-let getContests _request =  
- failwith  "getContests" 
+open Lwt.Infix
+open Redis_lwt
+
+let getContestsIdScoreboard _request = failwith "getContestsIdScoreboard"
+
+let getContestsContestIdSubmissions _request =
+  failwith "getContestsContestIdSubmissions"
+
+let postContestsContestsIdProblems _request =
+  failwith "postContestsContestsIdProblems"
+
+let getContestsContestsIdProblems _request =
+  failwith "getContestsContestsIdProblems"
+
+let deleteContestsId request =
+  let id = Dream.param request "id" in
+  Lwt_pool.use Db.pool(fun conn -> Client.del conn [("contest:"^id)]) >>= function 
+  | 0 -> Dream.html "nao existe"
+  | 1 -> Dream.html "deleted"
+  | _ -> Dream.html "errpr"
+
+let putContestsId _request = failwith "putContestsId"
+
+let getContestsId request =
+  let id = Dream.param request "id" in
+  Lwt_pool.use Db.pool (fun conn -> Client.hgetall conn ("contest:" ^ id))
+  >>= function
+  | [] -> Dream.html "nao existe"
+  | x ->
+      Dream.html (String.concat "" (List.map (fun (k, v) -> k ^ ":" ^ v) x))
+
+let postContests _request = failwith "postContests"
+
+let getContests _request = 
+  Lwt_pool.use Db.pool (fun conn -> Client.scan conn 0 ~pattern:"contest:*")
+  >>= function
+  | _,[] -> Dream.html "nao existe"
+  | _,x ->
+      Dream.html (String.concat "" x)
--- src/users.ml
+++ src/users.ml
@@ -1,10 +1,77 @@
-let deleteUsersId _request =  
- failwith  "deleteUsersId" 
-let putUsersId _request =  
- failwith  "putUsersId" 
-let getUsersId _request =  
- failwith  "getUsersId" 
-let postUsers _request =  
- failwith  "postUsers" 
-let getUsers _request =  
- failwith  "getUsers" 
+open Lwt.Infix
+open Redis_lwt
+
+let html_to_string html = Format.asprintf "%a" (Tyxml.Html.pp_elt ()) html
+
+let list_to_string l =
+  l |> List.map (fun (k, v) -> k ^ ": " ^ v) |> String.concat "\n"
+
+let deleteUsersId request =
+  let id = Dream.param request "id" in
+  Lwt_pool.use Db.pool (fun conn -> Client.del conn ["user:" ^ id])
+  >>= function 
+  | 1 ->  
+    Dream.response ~status:`OK
+      ~headers:[("Content-Type", "text/plain")]
+      "dleted"
+    |> Lwt.return
+  | 0 | _ ->     Dream.response ~status:`Not_Found
+      ~headers:[("Content-Type", "text/plain")]
+      "User not found\n"
+    |> Lwt.return
+
+let putUsersId request =
+  let id = Dream.param request "id" in
+  Dream.body request
+  >>= fun _update ->
+  Lwt_pool.use Db.pool (fun conn ->
+      Client.scan conn 0 ~pattern:("user:" ^ id)
+      >>= function
+      | _, [] ->
+          Dream.response ~status:`OK
+            ~headers:[("Content-Type", "text/plain")]
+            "User not found\n"
+          |> Lwt.return
+      | _, [x] -> (
+          Client.hgetall conn x
+          >>= function
+          | [] ->
+              Dream.response ~status:`Not_Found
+                ~headers:[("Content-Type", "text/plain")]
+                "User not found\n"
+              |> Lwt.return
+          | data ->
+              (* use hset here to update user with _update*)
+              Dream.response ~status:`OK
+                ~headers:[("Content-Type", "text/plain")]
+                (list_to_string data)
+              |> Lwt.return )
+      | _ ->
+          Dream.response ~status:`Bad_Request
+            ~headers:[("Content-Type", "text/plain")]
+            "Error"
+          |> Lwt.return )
+
+let getUsersId request =
+  let id = Dream.param request "id" in
+  Lwt_pool.use Db.pool (fun conn -> Client.hgetall conn ("user:" ^ id))
+  >>= fun data ->
+  match data with
+  | [] ->
+      Dream.response ~status:`Not_Found
+        ~headers:[("Content-Type", "text/plain")]
+        "User not found\n"
+      |> Lwt.return
+  | _ ->
+      Dream.response ~status:`OK
+        ~headers:[("Content-Type", "text/plain")]
+        (list_to_string data)
+      |> Lwt.return
+
+let postUsers _request = failwith "postUsers"
+
+let getUsers _request =
+  Lwt_pool.use Db.pool (fun conn -> Client.scan conn 0 ~pattern:"user:*")
+  >>= fun r ->
+  let _, r' = r in
+  Dream.html (html_to_string (User.users ~users:r'))
