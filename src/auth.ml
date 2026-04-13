72ab821792b4a82e7343d7b025beb2c01fa988e4
Pedro Casais <152277946+pedrocasais@users.noreply.github.com>
2026-04-13 19:47:40 +0100

    fix issue with Client.scan

--- src/auth.ml
+++ src/auth.ml
@@ -3,6 +3,12 @@ open Redis_lwt
 
 let html_to_string html = Format.asprintf "%a" (Tyxml.Html.pp_elt ()) html
 
+let rec aux cursor acc conn pattern =
+  if cursor = 0 then Lwt.return acc
+  else
+    Client.scan conn cursor ~pattern
+    >>= fun (next, lst) -> aux next (acc @ lst) conn pattern
+
 let rec userExist conn target = function
   | [] -> Lwt.return false
   | h :: t ->
@@ -11,13 +17,16 @@ let rec userExist conn target = function
       if data = Some target then Lwt.return true else userExist conn target t
 
 let postAuthRegister request =
+  let pattern = "user:*" in
   Dream.body request
   >>= fun data ->
   let user = Types.user_of_json data in
   Lwt_pool.use Db.pool (fun conn ->
-      Client.scan conn 0 ~pattern:"user:*"
-      >>= fun (_, users) ->
-      userExist conn user.username users
+      Client.scan conn 0 ~pattern
+      >>= fun (next_cursor, users) ->
+      aux next_cursor users conn pattern
+      >>= fun allusers ->
+      userExist conn user.username allusers
       >>= fun res ->
       if res then
         Dream.html (html_to_string (Index.index ~param:"ja exites" ~request))
@@ -32,12 +41,15 @@ let postAuthRegister request =
         >>= fun _ -> Dream.redirect request "/" )
 
 let postAuthLogin request =
+  let pattern = "user:*" in
   Dream.body request
   >>= fun data ->
   let user = Types.user_of_json data in
   Lwt_pool.use Db.pool (fun conn ->
-      Client.scan conn 0 ~pattern:"user:*"
-      >>= fun (_, users) -> userExist conn user.username users )
+      Client.scan conn 0 ~pattern
+      >>= fun (next_cursor, users) ->
+      aux next_cursor users conn pattern
+      >>= fun allusers -> userExist conn user.username allusers )
   >>= fun res ->
   if res then
     (*existe entra*)
--- src/users.ml
+++ src/users.ml
@@ -3,75 +3,66 @@ open Redis_lwt
 
 let html_to_string html = Format.asprintf "%a" (Tyxml.Html.pp_elt ()) html
 
-let list_to_string l =
-  l |> List.map (fun (k, v) -> k ^ ": " ^ v) |> String.concat "\n"
+let rec aux conn cursor acc pattern =
+  if cursor = 0 then Lwt.return acc
+  else
+    Client.scan conn cursor ~pattern
+    >>= fun (next_cursor, lst) -> aux conn next_cursor (acc @ lst) pattern
 
 let deleteUsersId request =
   let id = Dream.param request "id" in
   Lwt_pool.use Db.pool (fun conn -> Client.del conn ["user:" ^ id])
-  >>= function 
-  | 1 ->  
-    Dream.response ~status:`OK
-      ~headers:[("Content-Type", "text/plain")]
-      "dleted"
-    |> Lwt.return
-  | 0 | _ ->     Dream.response ~status:`Not_Found
-      ~headers:[("Content-Type", "text/plain")]
-      "User not found\n"
-    |> Lwt.return
+  >>= function
+  | 1 ->
+      Dream.response ~status:`OK
+        ~headers:[("Content-Type", "text/plain")]
+        "dleted"
+      |> Lwt.return
+  | 0 | _ ->
+      Dream.response ~status:`Not_Found
+        ~headers:[("Content-Type", "text/plain")]
+        "User not found\n"
+      |> Lwt.return
 
 let putUsersId request =
   let id = Dream.param request "id" in
   Dream.body request
   >>= fun _update ->
-  Lwt_pool.use Db.pool (fun conn ->
-      Client.scan conn 0 ~pattern:("user:" ^ id)
-      >>= function
-      | _, [] ->
-          Dream.response ~status:`OK
-            ~headers:[("Content-Type", "text/plain")]
-            "User not found\n"
-          |> Lwt.return
-      | _, [x] -> (
-          Client.hgetall conn x
-          >>= function
-          | [] ->
-              Dream.response ~status:`Not_Found
-                ~headers:[("Content-Type", "text/plain")]
-                "User not found\n"
-              |> Lwt.return
-          | data ->
-              (* use hset here to update user with _update*)
-              Dream.response ~status:`OK
-                ~headers:[("Content-Type", "text/plain")]
-                (list_to_string data)
-              |> Lwt.return )
-      | _ ->
-          Dream.response ~status:`Bad_Request
-            ~headers:[("Content-Type", "text/plain")]
-            "Error"
-          |> Lwt.return )
-
-let getUsersId request =
-  let id = Dream.param request "id" in
   Lwt_pool.use Db.pool (fun conn -> Client.hgetall conn ("user:" ^ id))
-  >>= fun data ->
-  match data with
+  >>= function
   | [] ->
       Dream.response ~status:`Not_Found
         ~headers:[("Content-Type", "text/plain")]
         "User not found\n"
       |> Lwt.return
   | _ ->
+      (*TODO: UPPDATE HERE ||*)
+      Dream.response ~status:`Not_Found
+        ~headers:[("Content-Type", "text/plain")]
+        "FOUND"
+      |> Lwt.return
+
+let getUsersId request =
+  let id = Dream.param request "id" in
+  Lwt_pool.use Db.pool (fun conn -> Client.hgetall conn ("user:" ^ id))
+  >>= function
+  | [] ->
+      Dream.response ~status:`Not_Found
+        ~headers:[("Content-Type", "text/plain")]
+        "User not found\n"
+      |> Lwt.return
+  | data ->
       Dream.response ~status:`OK
         ~headers:[("Content-Type", "text/plain")]
-        (list_to_string data)
+        (String.concat "" (List.map (fun (k, v) -> k ^ ": " ^ v) data))
       |> Lwt.return
 
 let postUsers _request = failwith "postUsers"
 
 let getUsers _request =
-  Lwt_pool.use Db.pool (fun conn -> Client.scan conn 0 ~pattern:"user:*")
-  >>= fun r ->
-  let _, r' = r in
-  Dream.html (html_to_string (User.users ~users:r'))
+  let pattern = "user:*" in
+  Lwt_pool.use Db.pool (fun conn -> Client.scan conn 0 ~pattern:pattern
+  >>= fun (cursor,users) ->
+  aux conn  cursor users pattern) >>= fun data -> 
+    
+  Dream.html (html_to_string (User.users ~users:data))
