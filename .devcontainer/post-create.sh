5aff95821cafa2ba83f92fdd615284720349bef1
Pedro Cardoso <pedrodamiao18@gmail.com>
2026-06-13 16:45:40 +0100

    Docker-api deps

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -10,6 +10,7 @@ opam install -y . --deps-only
 # Pin the atdgen packages to the latest version in the ahrefs/atd repository, which includes support for OCaml 5.0.
 opam pin add atdgen-runtime.dev https://github.com/ahrefs/atd.git
 opam pin add atdgen.dev https://github.com/ahrefs/atd.git
+opam pin add https://github.com/pedrodamiao18/ocaml-docker-api.git
 opam install jsonschema2atd
 
 opam install ocamlformat ocaml-lsp-server
